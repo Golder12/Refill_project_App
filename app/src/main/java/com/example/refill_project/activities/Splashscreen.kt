2e45176dcfa57331621196db7b922d22728756ac
Golder12 <golderalikaya@gmail.com>
2021-05-21 17:25:50 +0300

    more looks

--- app/src/main/java/com/example/refill_project/activities/Splashscreen.kt
+++ app/src/main/java/com/example/refill_project/activities/Splashscreen.kt
@@ -1,18 +1,30 @@
 package com.example.refill_project.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.view.WindowManager
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.refill_project.R
+import kotlinx.android.synthetic.main.activity_splashscreen.view.*
+
 
 class Splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        val yourLayout = findViewById<View>(R.id.splashscreen) as ConstraintLayout
+        val animationDrawable = yourLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(100)
+        animationDrawable.setExitFadeDuration(100)
+        animationDrawable.start()
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,

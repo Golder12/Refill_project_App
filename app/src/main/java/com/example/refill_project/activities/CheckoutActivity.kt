550f3fee66a8ff0c6a13d8a1459b1e03b4cf55c4
“ttcollins” <ttamalecollins@gmail.com>
2021-05-21 17:16:04 +0300

    major functionalities complete

--- app/src/main/java/com/example/refill_project/activities/CheckoutActivity.kt
+++ app/src/main/java/com/example/refill_project/activities/CheckoutActivity.kt
@@ -12,7 +12,6 @@ import com.example.refill_project.R
 import com.example.refill_project.UserInfo
 import com.example.refill_project.showToast
 import kotlinx.android.synthetic.main.activity_checkout.*
-import kotlinx.android.synthetic.main.login.*
 
 class CheckoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,5 +41,40 @@ class CheckoutActivity : AppCompatActivity() {
             }
         }
         rq.add(sr)
+
+        cancel.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        putorder.setOnClickListener {
+            var url = "http://refillug.rf.gd/android/placeorder/" + UserInfo.userid.toString()
+            var rq: RequestQueue = Volley.newRequestQueue(this)
+            var sr = object : StringRequest(Request.Method.GET, url, { response ->
+
+                if (response.equals("0")) {
+                    showToast("You cant make an order right now")
+                } else {
+                    UserInfo.userid = Integer.parseInt(response)
+                    var intent = Intent(this, OrderActivity::class.java)
+                    var msg = "Success! We shall deliver your order soon."
+                    intent.putExtra("msg", msg)
+                    startActivity(intent)
+                }
+
+            }, { error -> showToast(error.message!!) }) {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers.put(
+                        "Cookie",
+                        "__test=53e7f0ab4c8aeefdcec6b8b9e8d43286; expires=Friday, January 1, 2038 at 2:55:55 AM; path=/"
+                    );
+
+                    return headers
+                }
+            }
+            rq.add(sr)
+        }
     }
 }

b6ba5a04b957c825b15cf784d6446b58515237d4
“ttcollins” <ttamalecollins@gmail.com>
2021-05-21 07:28:43 +0300

    change of cookie

--- app/src/main/java/com/example/refill_project/QtyFragment.kt
+++ app/src/main/java/com/example/refill_project/QtyFragment.kt
@@ -72,7 +72,7 @@ class QtyFragment : DialogFragment() {
                         @Throws(AuthFailureError::class)
                         override fun getHeaders(): Map<String, String> {
                             val headers = HashMap<String, String>()
-                            headers.put("Cookie", "__test=2b653bde7094ac2751ce8aaf5d0aaa0b; expires=Friday, January 1, 2038 at 2:55:55 AM; path=/");
+                            headers.put("Cookie", "__test=53e7f0ab4c8aeefdcec6b8b9e8d43286; expires=Friday, January 1, 2038 at 2:55:55 AM; path=/");
 
                             return headers
                         }
